e9e6c22f4f5c36d340273dfc858b448b3682588f
Kazuki <kazuone0321@icloud.com>
2024-02-29 16:11:03 +0900

    first commit

--- clean.py
+++ clean.py
@@ -11,7 +11,6 @@ selected_assets = editor_util.get_selected_assets()
 num_assets = len(selected_assets)
 cleaned = 0
 
-# hard core parent path 
 parent_dir = "\\Game"
 
 if num_assets > 0:
@@ -19,12 +18,12 @@ if num_assets > 0:
       parent_dir = os.path.dirname(asset_path)
 
 for asset in selected_assets:
-        # get the class instance and the clear text name 
+        # アセットのクラスインスタンスとクリアテキスト名を取得
         asset_name = system_lib.get_object_name(asset)
         asset_class = asset.get_class()
         class_name = system_lib.get_class_display_name(asset_class)
 
-        # assemble new path and ewlocate assets
+        # 新しいパスを組み立ててアセットを移動
         try:
             new_path = os.path.join(parent_dir, class_name, asset_name)
             editor_asset_lib.rename_loaded_asset(asset, new_path)
@@ -32,6 +31,7 @@ for asset in selected_assets:
             unreal.log("Cleaned up {} to {}".format(asset_name, new_path))
         
         except Exception as error:
-              unreal.log("Could not move {} to new location {}".format(asset_name, new_path))
+            # 移動できなかった場合はエラーログを出力
+            unreal.log("Could not move {} to new location {}".format(asset_name, new_path))
 
 unreal.log("Cleaned up {} of {} assets".format(cleaned, num_assets))
